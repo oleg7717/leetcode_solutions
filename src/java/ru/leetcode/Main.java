9e8ab2ffee74875c82668c6f0ccea8043aad787d
ogoncharenko <ogoncharenko@interprocom.ru>
2025-10-26 11:00:59 +0300

    Word search solution

--- src/java/ru/leetcode/Main.java
+++ src/java/ru/leetcode/Main.java
@@ -2,5 +2,33 @@ package ru.leetcode;
 
 public class Main {
 	public static void main(String[] args) {
+		char[][] board0 = {"a".toCharArray()};
+		String word0 = "a";
+		char[][] board1 = {"ABCE".toCharArray(),"SFCS".toCharArray(),"ADEE".toCharArray()};
+		String word1 = "ABCCED";
+		char[][] board2 = {"CAA".toCharArray(),"AAA".toCharArray(),"BCD".toCharArray()};
+		String word2 = "AAB";
+		char[][] board3 = {"ABCE".toCharArray(), "SFCS".toCharArray(), "ADEE".toCharArray()};
+		String word3 = "SEE";
+		char[][] board4 = {"ABCE".toCharArray(), "SFCS".toCharArray(), "ADEE".toCharArray()};
+		String word4 = "ABCB";
+		char[][] board5 = {"aaaa".toCharArray(),"aaaa".toCharArray(),"aaaa".toCharArray()};
+		String word5 = "aaaaaaaaaaaaa";
+		char[][] board6 = { "aabaab".toCharArray(),
+							"aabbba".toCharArray(),
+							"aaaaba".toCharArray(),
+							"babbab".toCharArray(),
+							"abbaba".toCharArray(),
+							"baaaab".toCharArray()};
+		String word6 = "bbbaabbbbbab";
+
+		System.out.println("board 0: " + new Solution().exist(board0, word0));
+		System.out.println("board 1: " + new Solution().exist(board1, word1));
+		System.out.println("board 2: " + new Solution().exist(board2, word2));
+		System.out.println("board 3: " + new Solution().exist(board3, word3));
+		System.out.println("board 4: " + new Solution().exist(board4, word4));
+		System.out.println("board 5: " + new Solution().exist(board5, word5));
+		System.out.println("board 6: " + new Solution().exist(board6, word6));
+
 	}
 }
--- src/java/ru/leetcode/Solution.java
+++ src/java/ru/leetcode/Solution.java
@@ -1,4 +1,56 @@
 package ru.leetcode;
 
 public class Solution {
+	private char[][] board;
+	private String word;
+	private int boardHight;
+	private int boardWeight;
+	private int[] prev = new int[56];
+
+	public boolean exist(char[][] board, String word) {
+		if (word.length() > board.length * board[0].length) return false;
+		this.word = word;
+		this.board = board;
+		boardHight = board.length - 1;
+		boardWeight = board[0].length - 1;
+		int backward;
+		char firstChar = word.charAt(0);
+		for (int i = 0; i <= boardHight / 2; i++) {
+			backward = boardHight - i;
+			if (i > backward) {
+				return false;
+			}
+			for (int j = 0; j <= boardWeight; j++) {
+				if (i == backward && j > boardWeight - j) {
+					return false;
+				}
+
+				if (board[i][j] == firstChar) {
+					if (word.length() == 1 || nextChar(i, j, 1)) return true;
+				}
+				if (board[backward][boardWeight - j] == firstChar) {
+					if (word.length() == 1 || nextChar(backward, boardWeight - j, 1)) return true;
+				}
+			}
+		}
+
+		return false;
+	}
+
+	private boolean nextChar(int i, int j, int charPos) {
+		boolean lastChar = word.length() == charPos + 1;
+		char currChar = word.charAt(charPos);
+		prev[10 * i + j] = 1;
+		if (i < boardHight && prev[(i + 1) * 10 + j] == 0 && board[i + 1][j] == currChar
+				&& (lastChar || nextChar(i + 1, j, charPos + 1))) return true;
+		if (j < boardWeight && prev[i * 10 + j + 1] == 0 && board[i][j + 1] == currChar
+				&& (lastChar || nextChar(i, j + 1, charPos + 1))) return true;
+		if (i > 0 && prev[(i - 1) * 10 + j] == 0 && board[i - 1][j] == currChar
+				&& (lastChar || nextChar(i - 1, j, charPos + 1))) return true;
+		if (j > 0 && prev[i * 10 + j - 1] == 0 && board[i][j - 1] == currChar
+				&& (lastChar || nextChar(i, j - 1, charPos + 1))) return true;
+		prev[10 * i + j] = 0;
+
+		return false;
+	}
 }
