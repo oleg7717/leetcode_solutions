e9b78d09fed5948f0f025328c0cb81a085ebdc21
ogoncharenko <ogoncharenko@interprocom.ru>
2025-10-31 10:02:10 +0300

    Non-overlapping Intervals with quick sort

--- src/java/ru/leetcode/Main.java
+++ src/java/ru/leetcode/Main.java
@@ -2,5 +2,17 @@ package ru.leetcode;
 
 public class Main {
 	public static void main(String[] args) {
+		int[][] intervals1 = {{1,2},{2,3},{3,4},{1,3}};
+		int[][] intervals2 = {{1,2},{1,2},{1,2}};
+		int[][] intervals3 = {{1,2},{2,3}};
+		int[][] intervals4 = {{1,100},{1,11},{2,12},{11,22}};
+		int[][] intervals5 = {{0,2},{1,3},{2,4},{3,5},{4,6}};
+		int[][] intervals6 = {{0,2},{1,3},{1,3},{2,4},{3,5},{3,5},{4,6}};
+		System.out.println(new Solution().eraseOverlapIntervals(intervals1));
+		System.out.println(new Solution().eraseOverlapIntervals(intervals2));
+		System.out.println(new Solution().eraseOverlapIntervals(intervals3));
+		System.out.println(new Solution().eraseOverlapIntervals(intervals4));
+		System.out.println(new Solution().eraseOverlapIntervals(intervals5));
+		System.out.println(new Solution().eraseOverlapIntervals(intervals6));
 	}
 }
--- src/java/ru/leetcode/Solution.java
+++ src/java/ru/leetcode/Solution.java
@@ -1,4 +1,68 @@
 package ru.leetcode;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Solution {
+	public int eraseOverlapIntervals(int[][] intervals) {
+		if (intervals.length == 1) return 0;
+
+		quickSort(intervals, 0, intervals.length - 1);
+		int count = 0;
+		for (int i = intervals.length - 1; i >= 0; i--) {
+			if (i - 1 >= 0 && intervals[i][0] < intervals[i - 1][1]) {
+				intervals[i - 1] = intervals[i];
+				count++;
+			}
+		}
+
+		return count;
+	}
+
+	private void quickSort(int[][] intervals, int low, int high) {
+		List<Integer> stack = new ArrayList<>();
+		stack.add(low);
+		stack.add(high);
+		while (!stack.isEmpty()) {
+			high = stack.removeLast();
+			low = stack.removeLast();
+
+			if (low >= high) return;
+
+			int middle = low + (high - low) / 2;
+			int pivot = intervals[middle][0];
+
+			int i = low;
+			int j = high;
+
+			while (i < j) {
+				while (intervals[i][0] < pivot && i <= j) {
+					i++;
+				}
+
+				while (intervals[j][0] > pivot && i <= j) {
+					j--;
+				}
+
+				if (i <= j) {
+					if ((i != j && intervals[i][0] != intervals[j][0])) {
+						int[] temp = intervals[i];
+						intervals[i] = intervals[j];
+						intervals[j] = temp;
+					}
+					i++;
+					j--;
+				}
+			}
+
+			if (low < j) {
+				stack.add(low);
+				stack.add(j);
+			}
+			if (i < high) {
+				stack.add(i);
+				stack.add(high);
+			}
+		}
+	}
 }
