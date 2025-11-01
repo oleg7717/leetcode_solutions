8c82506e989825986b87bbf9d790396b0145430e
ogoncharenko <ogoncharenko@interprocom.ru>
2025-11-01 20:54:47 +0300

    Solution of task Longest Substring Without Repeating Characters

--- src/java/ru/leetcode/Solution.java
+++ src/java/ru/leetcode/Solution.java
@@ -1,4 +1,31 @@
 package ru.leetcode;
 
-public class Solution {
+class Solution {
+	public int lengthOfLongestSubstring(String s) {
+		if (s.isEmpty()) return 0;
+		int max = 1;
+		int lngth = s.length();
+		int size = 100;
+		char[] chars = new char[size];
+		char ch;
+		for (int i = 0; i < lngth; i++) {
+			if (max > lngth) break;
+			ch = s.charAt(i);
+			for (int j = 0; j < lngth; j++) {
+				int next = j + 1;
+				if (chars[j] == 0) {
+					chars[j] = ch;
+					if ((next) > max) {
+						max = next;
+					}
+					break;
+				} else if (chars[j] != ch) continue;
+				if (chars[j] == ch){
+					System.arraycopy(chars, next, chars, 0, size - (next));
+					j = -1;
+				}
+			}
+		}
+		return max;
+	}
 }
