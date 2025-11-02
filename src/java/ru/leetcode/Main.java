d7ac92c8541a0c4b934e069694b05a6d798295b0
ogoncharenko <ogoncharenko@interprocom.ru>
2025-11-02 14:29:42 +0300

    Solution of task Missing Number 0ms

--- src/java/ru/leetcode/Main.java
+++ src/java/ru/leetcode/Main.java
@@ -2,5 +2,11 @@ package ru.leetcode;
 
 public class Main {
 	public static void main(String[] args) {
+		int[] nums1 = {3,0,1};
+		int[] nums3 = {0,1};
+		int[] nums2 = {9,6,4,2,3,5,7,0,1};
+		System.out.println(new Solution().missingNumber(nums1));
+		System.out.println(new Solution().missingNumber(nums2));
+		System.out.println(new Solution().missingNumber(nums3));
 	}
 }
--- src/java/ru/leetcode/Solution.java
+++ src/java/ru/leetcode/Solution.java
@@ -1,4 +1,12 @@
 package ru.leetcode;
 
 public class Solution {
+	public int missingNumber(int[] nums) {
+		if (nums.length == 1) return nums[0] == 0 ? 1 : 0;
+		int missingNumber = 0;
+		for (int i = 0; i <= nums.length - 1; i++) {
+			missingNumber = missingNumber - nums[i] + (i + 1);
+		}
+		return missingNumber;
+	}
 }
