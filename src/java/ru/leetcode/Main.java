34bdcf7dac1b1a4f1313ae1a330fe0a76e482d11
ogoncharenko <ogoncharenko@interprocom.ru>
2025-11-02 11:09:37 +0300

    Solution of task Best Time to Buy and Sell Stock

--- src/java/ru/leetcode/Main.java
+++ src/java/ru/leetcode/Main.java
@@ -2,5 +2,9 @@ package ru.leetcode;
 
 public class Main {
 	public static void main(String[] args) {
+		int[] stocks1 = {2,11,1,4,7};
+		int[] stocks2 = {7,1,5,3,6,4};
+		System.out.println(new Solution().maxProfit(stocks1));
+		System.out.println(new Solution().maxProfit(stocks2));
 	}
 }
--- src/java/ru/leetcode/Solution.java
+++ src/java/ru/leetcode/Solution.java
@@ -1,4 +1,26 @@
 package ru.leetcode;
 
 public class Solution {
+	public int maxProfit(int[] prices) {
+		int minPrice = 10000;
+		int maxPrice = 0;
+		int maxProfit = 0;
+
+		for (int i = 0; i < prices.length; i++) {
+			int j = prices.length - 1;
+			if (i > j) break;
+			if (prices[i] < minPrice) {
+				minPrice = prices[i];
+			} else if (prices[i] - minPrice > maxProfit) {
+				maxProfit = prices[i] - minPrice;
+			}
+			if (prices[j] > maxPrice) {
+				maxPrice = prices[j];
+			} else if (maxPrice - prices[j] > maxProfit) {
+				maxProfit = maxPrice - prices[j];
+			}
+		}
+
+		return maxProfit;
+	}
 }
