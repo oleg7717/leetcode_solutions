ff518eb8b069808e2ddbd9f60175c76ee19aed16
ogoncharenko <ogoncharenko@interprocom.ru>
2025-11-22 12:38:09 +0300

    Solution of task Number of Islands

--- src/java/ru/leetcode/Main.java
+++ src/java/ru/leetcode/Main.java
@@ -2,5 +2,13 @@ package ru.leetcode;
 
 public class Main {
 	public static void main(String[] args) {
+		char[][] grid = {
+				"11000".toCharArray(),
+				"11000".toCharArray(),
+				"00100".toCharArray(),
+				"00011".toCharArray()
+			};
+
+		System.out.println(new Solution().numIslands(grid));
 	}
 }
--- src/java/ru/leetcode/Solution.java
+++ src/java/ru/leetcode/Solution.java
@@ -1,4 +1,34 @@
 package ru.leetcode;
 
-public class Solution {
+class Solution {
+	public int numIslands(char[][] grid) {
+		int rows = grid.length;
+		int cols = grid[0].length;
+
+		int islandCount = 0;
+
+		for (int row = 0; row < rows; row++) {
+			for (int col = 0; col < cols; col++) {
+				if (grid[row][col] == '1') {
+					dfs(grid, row, col);
+					islandCount++;
+				}
+			}
+		}
+
+		return islandCount;
+	}
+
+	private void dfs(char[][] grid, int r, int c) {
+		int row = grid.length;
+		int col = grid[0].length;
+
+		if (r < 0 || r >= row || c < 0 || c >= col || grid[r][c] != '1') return;
+		grid[r][c] = '0';
+
+		dfs(grid , r + 1 , c);
+		dfs(grid , r , c + 1);
+		dfs(grid , r - 1 , c);
+		dfs(grid , r , c - 1);
+	}
 }
